4ac477fe10ca2eef858513f1cf4b0ffe34de6b55
seoyeon-baek <w2032@e-mirim.hs.kr>
2021-11-27 17:27:35 +0900

    Add data.js

---
+++ part03/part03/data/data.js
@@ -0,0 +1,76 @@
+window.All = {
+    'people':[
+        {
+            'id':'no01',
+            'name':'people01',
+            'title':'peopleTitle',
+            'bio':'people01 설명',
+            'img':'./images/people_01.png',
+            'order':0,
+            'title_car':'Office'
+        },
+        {
+            'id':'no32',
+            'name':'people02',
+            'title':'peopleTitle',
+            'bio':'people02 설명',
+            'img':'./images/people_02.png',
+            'order':0,
+            'title_car':'Office'
+        },
+        {
+            'id':'no03',
+            'name':'people03',
+            'title':'peopleTitle',
+            'bio':'people03 설명',
+            'img':'./images/people_03.png',
+            'order':0,
+            'title_car':'Office'
+        },
+        {
+            'id':'no04',
+            'name':'people04',
+            'title':'peopleTitle',
+            'bio':'people04 설명',
+            'img':'./images/people_04.png',
+            'order':0,
+            'title_car':'Office'
+        },
+        {
+            'id':'no05',
+            'name':'people05',
+            'title':'peopleTitle',
+            'bio':'people05 설명',
+            'img':'./images/people_05.png',
+            'order':0,
+            'title_car':'Office'
+        },
+        {
+            'id':'no06',
+            'name':'people06',
+            'title':'peopleTitle',
+            'bio':'people06 설명',
+            'img':'./images/people_06.png',
+            'order':0,
+            'title_car':'Office'
+        },
+        {
+            'id':'no07',
+            'name':'people07',
+            'title':'peopleTitle',
+            'bio':'people07 설명',
+            'img':'./images/people_07.png',
+            'order':0,
+            'title_car':'Office'
+        },
+        {
+            'id':'no08',
+            'name':'people08',
+            'title':'peopleTitle',
+            'bio':'people08 설명',
+            'img':'./images/people_08.png',
+            'order':0,
+            'title_car':'Office'
+        },
+    ]
+}
