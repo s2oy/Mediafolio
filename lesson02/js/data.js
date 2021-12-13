b5b01ee46f41ce1a35ec10bbb38ffab579d461e0
백서연 <w2032@e-mirim.hs.kr>
2021-12-13 23:59:42 +0900

    Add navbar, aside animation and Fix media query

--- lesson02/js/data.js
+++ lesson02/js/data.js
@@ -1,20 +1,54 @@
 function navScroll(){
-    // nav
-    $('nav>ul>li>a').on('click',function(e){
-        let str = $(this).attr('href');
-        // console.log(str, headerH);
-        let navPos = $(str).offset().top;
-        // console.log(navPos);
-        $('body,html').animate({scrollTop:navPos},1000);
-        return false;
-    });
+    // nav // 
+ $('nav>ul>li>a').on('click',function(e){
+     let str = $(this).attr('href');
+    let navPos = $(str).offset().top;
+    let windowW = $('#wrap').width();
+    // console.log(navPos);
+    if(windowW < 980){
+        let navW = $('nav').innerWidth();
+        // $('nav').hide();
+        $('#btn').show();
+        $('nav').css('left','-'+navW+'px');
+    }
+    $('body,html').animate({scrollTop:navPos},1000);
+    // resize() 문제 해결
+    //  
+    return false;
+ });
+//  aside>ul>li>a
+$('aside>ul>li>a').on('click',function(e){
+    let str = $(this).attr('href');
+    let navPos = $(str).offset().top;
+    $('body,html').animate({scrollTop:navPos},1000);
 
-    $('aside>ul>li>a').on('click', function(e){
-        let str = $(this).attr('href');
-        let navPos = $(str).offset().top;
-        $('body,html').animate({scrollTop:navPos},1000);
-        return false;
+});
+
+$('#top').on('click',function(e){
+    $('body,html').animate({scrollTop:0},1000);
+});
+// top
+}
+
+function navBtn(){
+    $('#btn').on('click',function(e){
+        $(this).hide();
+        $('nav').animate({left:0},400);
     });
-    $('#top').on('click',function(){});
-    //top
+    $('nav .close').on('click',function(e){
+        let navWidth = $('nav').innerWidth();
+        console.log(navWidth);
+        $('nav').animate({left:'-'+navWidth+'px'},400,function(){
+            $('#btn').show();
+        });
+
+    })
+}
+
+// 
+function gallery(){
+//  준비하기
+
+//  .left  .right
+
 }
